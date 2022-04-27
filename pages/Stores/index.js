5a1e703df15f111b4102def357cf21329de07b4d
Aayush Bhagat <lamblesus@gmail.com>
2022-04-27 19:45:57 -0400

    Intial outline of webapp

---
+++ pages/Stores/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Card} from 'react-bootstrap'
+import styles from '../../styles/Places.module.css'
+
+export default function index() {
+  return (
+    <>
+        <div className={styles.Places}>
+            <div className={styles.titleContainer}>
+                <h2 className={styles.title}>Stores</h2>
+            </div>
+            <div className={styles.filterContainer}>
+                <input className={styles.search} placeholder='Search' type="text" />
+                  <select placeholder='Categories' name="Tags" className={styles.categories}>
+                      <option value="" disabled selected hidden>Categories</option>
+                      <option value="Any">Any</option>
+                      <option value="Italian">Italian</option>
+                      <option value="Fast">Fast</option>
+                      <option value="Dessert">Dessert</option>
+                  </select>
+            </div>
+            <div className={styles.cardContainer}>
+                  <Card className={styles.cards}>
+                      <img width="90px" height="90px" src="https://corporate.target.com/_media/TargetCorp/Press/B-roll%20and%20Press%20Materials/Logos/Target_Bullseye-Logo_Red.jpg?preset=640w" alt="" />
+                      <Card.Body className={styles.body}>
+                          <Card.Title>Target</Card.Title>
+                          <p className={styles.address}>367 Russell St, Hadley, MA 01035</p>
+                          <div className={styles.tags}>
+                              <div className={styles.tag} style={{ backgroundColor:'#63BDFF'}}>Groceries</div>
+                              <div className={styles.tag} style={{ backgroundColor:'#FF6363'}}>School</div>
+                              <div className={styles.tag} style={{ backgroundColor: '#FFC163' }}>Tech</div>
+                              <div className={styles.tag} style={{ backgroundColor: '#9E63FF' }}>Clothes</div>
+                          </div>
+                      </Card.Body>
+                  </Card>
+            </div>
+        </div>
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Navbar, Container, Nav} from 'react-bootstrap'
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return(
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+            <Link href="/Stores" passHref><Nav.Link>Stores</Nav.Link></Link>
+            <Link href="/Restaurants" passHref><Nav.Link>Restaurants</Nav.Link></Link>
+            <Link href="/Entertainment" passHref><Nav.Link>Entertainment</Nav.Link></Link>
+            <Link href="#favorites" passHref><Nav.Link >Favorites</Nav.Link></Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+
+  )
 }
 
 export default MyApp
