bc64ffb835e8fc8c8e92551f8b8805412b3214d1
Aayush Bhagat <lamblesus@gmail.com>
2022-05-04 18:59:15 -0400

    Added Favorites page

--- pages/Favorites/index.js
+++ pages/Favorites/index.js
@@ -55,8 +55,11 @@ export default function Index() {
         )
     }
   return (
-    <div>
-        {favs === null? null : renderFavs(favs)}
-    </div>
+      <>
+          <h1> Favorites</h1>
+          <div>
+              {favs === null ? null : renderFavs(favs)}
+          </div>
+      </>
   )
 }
